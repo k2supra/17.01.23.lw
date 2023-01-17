98b388367160e17e6a04ced2a5f66eeb040fe3ef
k2supra <andrijkorolevic@gmail.com>
2023-01-17 20:35:40 +0200

    1-2

--- main.cpp
+++ main.cpp
@@ -1,4 +1,88 @@
 #include <iostream>
+
 using namespace std;
 
- 
+int main() {
+	cout << "Enter a number of operation(1-2): ";
+	int operation;
+	cin >> operation;
+	switch (operation)
+	{
+	case 1: {
+		cout << "Enter a number: ";
+		uint16_t number;
+		cin >> number;
+		for (size_t i = 0; i < number + 1; i++)
+		{
+			cout << i << " ";
+		}
+	}break;
+	case 2: {
+		cout << "Enter start number: ";
+		uint16_t number_start;
+		cin >> number_start;
+		cout << "Enter finish number: ";
+		uint16_t number_finish;
+		cin >> number_finish;
+		cout << "Pair: ";
+		if (number_start > number_finish)
+		{
+			for (size_t i = number_finish; i < number_start + 1; i++)
+			{
+				if (i % 2 == 0)
+				{
+					cout << i << " ";
+				}
+			}
+			cout << "\n";
+			cout << "Unpair: ";
+			for (size_t i = number_finish; i < number_start + 1; i++)
+			{
+				if (i % 2 != 0)
+				{
+					cout << i << " ";
+				}
+			}
+			cout << "\n";
+			cout << "/7: ";
+			for (size_t i = number_finish; i < number_start + 1; i++)
+			{
+				if (i % 7 == 0)
+				{
+					cout << i << " ";
+				}
+			}break;
+		}
+		
+		cout << "\n" << "Pair: ";
+		for (size_t i = number_start; i < number_finish + 1; i++)
+		{
+			if (i % 2 == 0)
+			{
+				cout << i << " ";
+			}
+		}
+		cout << "\n";
+		cout << "Unpair: ";
+		for (size_t i = number_start; i < number_finish + 1; i++)
+		{
+			if (i % 2 != 0)
+			{
+				cout << i << " ";
+			}
+		}
+		cout << "\n";
+		cout << "/7: ";
+		for (size_t i = number_start; i < number_finish + 1; i++)
+		{
+			if (i % 7 == 0)
+			{
+				cout << i << " ";
+			}
+		}
+	}break;
+	default:
+		break;
+	}
+	
+}
