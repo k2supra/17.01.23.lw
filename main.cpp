4836bf8d20fc862a4d6c78264395750a66b73197
k2supra <andrijkorolevic@gmail.com>
2023-01-18 14:07:06 +0200

    )

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
-#include <iostream>
+﻿#include <iostream>
+#include <time.h>
+#include <Windows.h>
+#include <io.h>
+#include <stdio.h>
+#include <fcntl.h>
+
 
 using namespace std;
 
 int main() {
-	cout << "Enter a number of operation(1-4): ";
+	srand(time(NULL));
+	cout << "Enter a number of operation(1-6): ";
 	int operation;
 	cin >> operation;
 	switch (operation)
@@ -53,7 +60,7 @@ int main() {
 				}
 			}break;
 		}
-		
+
 		cout << "\n" << "Pair: ";
 		for (size_t i = number_start; i < number_finish + 1; i++)
 		{
@@ -105,14 +112,181 @@ int main() {
 		{
 			cout << "Enter a number: ";
 			cin >> number;
-			
+
 			sum += number;
 		} while (number != 0);
-		
+
 		cout << " = " << sum;
-	}
+	}break;
+	case 5: {
+		unsigned short rnum = rand() % 500;
+		cout << "Enter a number: ";
+		int mnum;
+		cin >> mnum;
+		int counter = 2;
+		do
+		{
+			cout << "Enter a number: ";
+			cin >> mnum;
+			if (mnum < rnum)
+			{
+				cout << "\nYour number is less than random ";
+				counter += 1;
+			}
+			else if (mnum > rnum)
+			{
+				cout << "\nYour number is more than random ";
+				counter += 1;
+			}
+
+		} while (mnum != rnum);
+		cout << counter << " tries.";
+		cout << " You did it!" << " Random number is " << rnum;
+	}break;
+	case 6: {
+		/*::_setmode(::_fileno(stdout), _O_U16TEXT);
+		auto const & sz_message
+		{
+			L"███████████████████████████████████" L"\n"
+			L"█     ██████   ██████ ████ █ ██ ███" L"\n"
+			L"█  ███ ████ ███ █████  ███ █ █ ████" L"\n"
+			L"█     ████ █████ ████ █ ██ █  █████" L"\n"
+			L"█  ███ ██         ███ ██ █ █ █ ████" L"\n"
+			L"█     ██ █████████ ██ ███  █ ██ ███" L"\n"
+			L"███████████████████████████████████" L"\n"
+		};*/
+		cout << "\t\tBAN K";
+		cout << "\nChoose the value what you want to change:";
+		cout << "\n Grivn`a   -   1";
+		cout << "\n Dollar    -   2";
+		cout << "\n Rupee     -   3";
+		cout << "\n Brawl gem -   4";
+		cout << "\nNumber: ";
+		int oper;
+		cin >> oper;
+		system("cls");
+		cout << "\nChoose the value what you want to change:";
+		cout << "\n Grivn`a   -   1";
+		cout << "\n Dollar    -   2";
+		cout << "\n Rupee     -   3";
+		cout << "\n Brawl gem -   4";
+		cout << "\nNumber: ";
+		int oper2;
+		cin >> oper2;
+		double grn, dollar, rupee, brawl_gem;
+		switch (oper)
+		{
+		case 1: {
+			switch (oper2)
+			{
+			case 1: {
+				cout << "Enter your money(grn): ";
+				cin >> grn;
+				cout << "You will get: " << grn << " grn.";
+			}break;
+			case 2: {
+				cout << "Enter your money(grn): ";
+				cin >> grn;
+				cout << "You will get: " << grn / 40 << " $.";
+			}break;
+			case 3: {
+				cout << "Enter your money(grn): ";
+				cin >> grn;
+				cout << "You will get: " << grn / 45 << " rupee.";
+			}break;
+			case 4: {
+				cout << "Enter your money(grn): ";
+				cin >> grn;
+				cout << "You will get: " << grn / 1.67 << " gems in BRAWL STARS.";
+			}break;
+
+			default:
+				break;
+			}
+		}break;
+		case 2: {
+			switch (oper2)
+			{
+			case 1: {
+				cout << "Enter your money($): ";
+				cin >> dollar;
+				cout << "You will get: " << dollar * 40 << " grn.";
+			}break;
+			case 2: {
+				cout << "Enter your money($): ";
+				cin >> dollar;
+				cout << "You will get: " << dollar << " $.";
+			}break;
+			case 3: {
+				cout << "Enter your money($): ";
+				cin >> dollar;
+				cout << "You will get: " << dollar * 113 << " rupee.";
+			}break;
+			case 4: {
+				cout << "Enter your money($): ";
+				cin >> dollar;
+				cout << "You will get: " << dollar * 0.066 << " gems in BRAWL STARS.";
+			}break;
+			}
+		default:
+			break;
+		}break;
+		case 3: {
+			switch (oper2)
+			{
+			case 1: {
+				cout << "Enter your money(rupee): ";
+				cin >> rupee;
+				cout << "You will get: " << rupee * 0.28 << " grn.";
+			}break;
+			case 2: {
+				cout << "Enter your money(rupee): ";
+				cin >> rupee;
+				cout << "You will get: " << rupee / 131 << " $.";
+			}break;
+			case 3: {
+				cout << "Enter your money(rupee): ";
+				cin >> rupee;
+				cout << "You will get: " << rupee << " rupee.";
+			}break;
+			case 4: {
+				cout << "Enter your money(rupee): ";
+				cin >> rupee;
+				cout << "You will get: " << rupee * 0.023 << " gems in BRAWL STARS.";
+			}break;
+			}break;
+		}
+		case 4: {
+			switch (oper2)
+			{
+			case 1: {
+				cout << "Enter your money(gem): ";
+				cin >> brawl_gem;
+				cout << "You will get: " << brawl_gem * 1.67 << " grn.";
+			}break;
+			case 2: {
+				cout << "Enter your money(gem): ";
+				cin >> brawl_gem;
+				cout << "You will get: " << brawl_gem / 0.66 << " $.";
+			}break;
+			case 3: {
+				cout << "Enter your money(gem): ";
+				cin >> brawl_gem;
+				cout << "You will get: " << brawl_gem / 0.23 << " rupee.";
+			}break;
+			case 4: {
+				cout << "Enter your money(gem): ";
+				cin >> brawl_gem;
+				cout << "You will get: " << brawl_gem << " gems in BRAWL STARS.";
+			}break;
+			}break;
+		}
+			/*::std::wcout << sz_message << ::std::flush;*/
+			return(0);
+		}
 	default:
 		break;
 	}
-	
+
+	}
 }
